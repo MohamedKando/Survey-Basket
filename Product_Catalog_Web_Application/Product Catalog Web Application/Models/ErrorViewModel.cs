477e8b6633d8fd81f85796c569c13b1e7fbd9ef4
Mohamed Kandil <137327154+MohamedKando@users.noreply.github.com>
2025-07-04 16:57:56 +0300

    Delete Product_Catalog_Web_Application directory

--- Product_Catalog_Web_Application/Product Catalog Web Application/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Product_Catalog_Web_Application.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
